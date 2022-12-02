f076ca38c8f47efa3d6f266f64f85aed382672f4
Michael Campbell <macbestcam@gmail.com>
2022-12-02 13:28:55 -0500

    fixed localhost

--- client/src/components/general/nav_bar.jsx
+++ client/src/components/general/nav_bar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Navbar() {
   return (
     <div className="r_nav">
-      <a href="https://umd-spotify-analytics.herokuapp.com/api/callback">Login</a>
+      <a href="https://umd-spotify-analytics.herokuapp.com/api/login">Login</a>
       <a href="/umd_stats">UMD Stats</a>
       <a href="/about_us">About us</a>
     </div>
