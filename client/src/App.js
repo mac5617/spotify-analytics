2703e36110963cc05fe03f6a3e77c600873d4fcb
Michael Campbell <macbestcam@gmail.com>
2022-12-02 23:54:31 -0500

    full setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
-import './App.css';
-import PreLogNav from './components/general/navbar/PreLogNav'
+import "./App.css";
+import PreLogNav from "./components/general/navbar/PreLogNav";
 const App = () => {
-
   return (
     <div>
       <div className="nav">
@@ -10,7 +9,6 @@ const App = () => {
           <p></p>
         </div>
         <PreLogNav />
-
       </div>
       <div className="quote">
         <p>
@@ -21,31 +19,108 @@ const App = () => {
       <div className="flex_container">
         <ul className="storing_information">
           Currently Storing:
-          <li><span className="effects_1" id="users">50</span> Users</li>
-          <li><span className="effects_1" id="songs">2200</span> Songs</li>
-          <li><span className="effects_1" id="artists">120</span> Artists</li>
-          <li><span className="effects_1" id="genres">32</span> Genres</li>
+          <li>
+            <span className="effects_1" id="users">
+              50
+            </span>{" "}
+            Users
+          </li>
+          <li>
+            <span className="effects_1" id="songs">
+              2200
+            </span>{" "}
+            Songs
+          </li>
+          <li>
+            <span className="effects_1" id="artists">
+              120
+            </span>{" "}
+            Artists
+          </li>
+          <li>
+            <span className="effects_1" id="genres">
+              32
+            </span>{" "}
+            Genres
+          </li>
         </ul>
         <div className="Artist-List">
           <div>
-            <img src="https://i.scdn.co/image/ab6761610000f178fd30ebd7e80dad6b2383aab0" alt="" />
-            <img src="https://i.scdn.co/image/ab6761610000f178b7097d4c2bc9751c14695351" alt="" />
-            <img src="https://i.scdn.co/image/080090a26f6df71cae1925b8b9f76a236b524b18" alt="" />
+            <div>
+              <h4>Name1</h4>
+              <img
+                src="https://i.scdn.co/image/ab6761610000f178fd30ebd7e80dad6b2383aab0"
+                alt=""
+              />
+            </div>
+            <div>
+              <h4>Name2</h4>
+              <img
+                src="https://i.scdn.co/image/ab6761610000f178b7097d4c2bc9751c14695351"
+                alt=""
+              />
+            </div>
+            <div>
+              <h4>Name3</h4>
+              <img
+                src="https://i.scdn.co/image/080090a26f6df71cae1925b8b9f76a236b524b18"
+                alt=""
+              />
+            </div>
           </div>
           <div>
-            <img src="https://i.scdn.co/image/ab6761610000f178fd30ebd7e80dad6b2383aab0" alt="" />
-            <img src="https://i.scdn.co/image/ab6761610000f178b7097d4c2bc9751c14695351" alt="" />
-            <img src="https://i.scdn.co/image/080090a26f6df71cae1925b8b9f76a236b524b18" alt="" />
+            <div>
+              <div>
+                <h4>Name1</h4>
+                <img
+                  src="https://i.scdn.co/image/ab6761610000f178fd30ebd7e80dad6b2383aab0"
+                  alt=""
+                />
+              </div>
+              <div>
+                <h4>Name2</h4>
+                <img
+                  src="https://i.scdn.co/image/ab6761610000f178b7097d4c2bc9751c14695351"
+                  alt=""
+                />
+              </div>
+              <div>
+                <h4>Name3</h4>
+                <img
+                  src="https://i.scdn.co/image/080090a26f6df71cae1925b8b9f76a236b524b18"
+                  alt=""
+                />
+              </div>
+            </div>
           </div>
           <div>
-            <img src="https://i.scdn.co/image/ab6761610000f178fd30ebd7e80dad6b2383aab0" alt="" />
-            <img src="https://i.scdn.co/image/ab6761610000f178b7097d4c2bc9751c14695351" alt="" />
-            <img src="https://i.scdn.co/image/080090a26f6df71cae1925b8b9f76a236b524b18" alt="" />
+            <div>
+              <div>
+                <h4>Name1</h4>
+                <img
+                  src="https://i.scdn.co/image/ab6761610000f178fd30ebd7e80dad6b2383aab0"
+                  alt=""
+                />
+              </div>
+              <div>
+                <h4>Name2</h4>
+                <img
+                  src="https://i.scdn.co/image/ab6761610000f178b7097d4c2bc9751c14695351"
+                  alt=""
+                />
+              </div>
+              <div>
+                <h4>Name3</h4>
+                <img
+                  src="https://i.scdn.co/image/080090a26f6df71cae1925b8b9f76a236b524b18"
+                  alt=""
+                />
+              </div>
+            </div>
           </div>
         </div>
       </div>
     </div>
-
   );
 };
 
